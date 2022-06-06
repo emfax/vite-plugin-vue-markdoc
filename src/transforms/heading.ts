0314d4ccd6fe342e2864242753c6e58c6aa0f90f
Matt Andrews <mattandrews@massey.com.au>
2022-06-06 10:44:56 +1000

    Add heading transformer and tests

---
+++ src/transforms/heading.ts
@@ -0,0 +1,47 @@
+import { Tag } from "@markdoc/markdoc";
+import type { Node, Schema } from "@markdoc/markdoc";
+
+// Or replace this with your own function
+function generateID(node: Node, attributes: { [key: string]: unknown }) {
+  if (attributes.id && typeof attributes.id === 'string') {
+    return attributes.id;
+  }
+
+  let id = "";
+
+  for(const child of node.walk()) {
+    if (child.type === "inline") continue;
+
+    if (child.type === "text") {
+      const text = child.attributes.content as string;
+
+      id = text.replace(/[.,]/g, "-")
+        .replace(/\s+/g, '-')
+        .replace(/-{2,}/g, '-')
+        .toLowerCase();
+    }
+  }
+
+  return id;
+}
+
+export const heading = {
+  children: ["inline"],
+  
+  attributes: {
+    id: { type: String },
+    level: { type: Number, required: true, default: 1 },
+  },
+  
+  transform(node: Node, config): Tag {
+    const children = node.transformChildren(config);
+
+    const id = generateID(node, node.attributes);
+
+    return new Tag(
+      `h${node.attributes['level']}`,
+      { ...(id && { id }) },
+      children,
+    );
+  }
+} as Schema;
---
+++ tests/transform-heading.test.ts
@@ -0,0 +1,20 @@
+import { readFile } from "node:fs/promises";
+import { beforeAll, describe, expect, test } from "vitest";
+import Markdoc from "@markdoc/markdoc"; 
+import { heading } from "../src/transforms/heading";
+
+let md = "";
+
+describe('heading transformer', () => {
+  beforeAll(async () => {
+    md = await readFile("tests/fixtures/heading.md", "utf-8");
+  });
+
+  test("the correct id is generated", () => {
+    const ast = Markdoc.parse(md);
+    const renderable = Markdoc.transform(ast, { nodes: { heading }});
+    const html = Markdoc.renderers.html(renderable);
+
+    expect(html).toMatchSnapshot();
+  });
+});
